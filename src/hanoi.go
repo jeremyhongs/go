cdd1e0f8d23b1c9976415ec51fb76964f05ffef0
jeremyhongs <seungeuih@gmail.com>
2018-06-01 13:44:41 +0900

    commit

---
+++ src/hanoi.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func move(n, from, to, via int) {
+	if n <= 0 {
+		return
+	}
+
+	move(n-1, from, via, to)
+	fmt.Println(from, "->", to)
+
+	move(n-1, via, to, from)
+}
+
+func hanoi(n int) {
+	fmt.Println("Number of disk : ", n)
+	move(n, 1, 2, 3)
+}
+
+func main() {
+	hanoi(3)
+}
